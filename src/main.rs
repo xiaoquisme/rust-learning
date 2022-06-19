ad4d45b3da5074492663b6a85e839a5f91e8e96e
XiaoQu <lqqu@thoughtworks.com>
2022-06-19 15:53:36 +0800

    feat: add cmp function test

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-
-mod my_enum_module;
 mod config;
+mod my_enum_module;
 use self::my_enum_module::my_struct::Queue;
+use std::cmp::Ordering;
 
 type my_queue = Queue<i32>;
 
- fn main() {
+fn main() {
     println!("{}", my_enum_module::enum_test::my_test());
     let mut queue = Queue::<i32>::new();
     let mut queue2 = my_queue::new();
@@ -13,6 +13,13 @@ type my_queue = Queue<i32>;
     println!("queue is empty: {}", queue.is_empty());
     config::print_config();
     println!("main");
+    let re = my_enum_module::enum_test::compare(1 as i32, 1 as i32);
+    let result = match re {
+        Ordering::Equal => 0,
+        Ordering::Greater => 1,
+        Ordering::Less => -1,
+    };
+    println!("{}", result);
 
- }
-
+    println!("{:?}", re.is_eq());
+}
--- src/my_enum_module/enum_test.rs
+++ src/my_enum_module/enum_test.rs
@@ -1,3 +1,16 @@
+use std::cmp::Ordering;
+// use std::cmp::Ordering::*;
+
 pub fn my_test() -> String {
     String::from("this is from xiaoqu")
+}
+
+pub fn compare(n: i32, m: i32) -> Ordering {
+    if n < m {
+        return Ordering::Less;
+    }
+    if n > m {
+        return Ordering::Greater;
+    }
+    Ordering::Equal
 }
